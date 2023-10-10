6e321b7dc69f838fe27bdfa2f48dadb1ea925762
s1f102101615 <s1f102101615@iniad.org>
2023-10-10 22:36:47 +0900

    page

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,22 +1,23 @@
 import { useAtom } from 'jotai';
-import type { ChangeEvent } from 'react';
-import { useState } from 'react';
 import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
 import { userAtom } from '../atoms/user';
 import styles from './index.module.css';
 
 const Home = () => {
   const [user] = useAtom(userAtom);
-  const [label, setLabel] = useState('');
-  const inputLabel = (e: ChangeEvent<HTMLInputElement>) => {
-    setLabel(e.target.value);
-  };
 
   return (
     <>
       <BasicHeader user={user} />
+      {/* 画像とモチベーティブな言葉のhead */}
       <div className={styles.head}>
-        {/* <Image src={back.src} /> */}
+        <img
+          className={styles.proimage}
+          src="/images/progame.jpg"
+          alt="My Image"
+          width={700}
+          height={500}
+        />
 
         <div className={styles.title} style={{}}>
           熱中した過去に
@@ -27,6 +28,10 @@ const Home = () => {
         </div>
       </div>
       {/* <div className={styles.backgroundphoto} /> */}
+      <div className={styles.pagedetail}>
+        Gamers新卒とは？ <br />
+        ~~~
+      </div>
       <div className={styles.detail}>
         <div style={{ fontSize: 30, fontWeight: 'bold' }}>登録できるゲーム</div>
         <div style={{ flexDirection: 'row', display: 'flex', gap: 30, marginTop: 30 }}>
@@ -41,10 +46,25 @@ const Home = () => {
           <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
           <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
         </div>
+        <div style={{ height: 75, marginTop: 60 }}>
+          <a href="" className={styles.yellow}>
+            新規登録はこちらから
+          </a>
+        </div>
+        <div>およそ3分程度で作成することが出来ます。</div>
       </div>
       {/* メールアドレスとパスワードを入力する登録 */}
       <div className={styles.submitarea}>
-        <div style={{ fontSize: 30, fontWeight: 30 }}>無料登録はこちら</div>
+        <div style={{ height: 75 }}>
+          <a href="" className={styles.yellow}>
+            新規登録はこちらから
+          </a>
+        </div>
+        <div>およそ3分程度で作成することが出来ます。</div>
+      </div>
+      {/* ご利用の流れ */}
+      <div className={styles.infoarea}>
+        <div>流れを書く</div>
       </div>
     </>
   );
