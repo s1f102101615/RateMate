349df37094b39c80f1e7567ac3048bf5266703a0
s1f102101615 <s1f102101615@iniad.org>
2023-10-11 15:29:46 +0900

    welcome page作成

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -11,13 +11,7 @@ const Home = () => {
       <BasicHeader user={user} />
       {/* 画像とモチベーティブな言葉のhead */}
       <div className={styles.head}>
-        <img
-          className={styles.proimage}
-          src="/images/progame.jpg"
-          alt="My Image"
-          width={700}
-          height={500}
-        />
+        <img className={styles.proimage} src="/images/progame.jpg" alt="My Image" width="100%" />
 
         <div className={styles.title} style={{}}>
           熱中した過去に
@@ -29,42 +23,73 @@ const Home = () => {
       </div>
       {/* <div className={styles.backgroundphoto} /> */}
       <div className={styles.pagedetail}>
-        Gamers新卒とは？ <br />
-        ~~~
+        <span style={{ fontSize: 40, fontWeight: 'bold', marginBottom: 15, color: 'green' }}>
+          Gamers新卒とは？ <br />
+        </span>
+        あなたのゲームのレートや実績に基づいて、企業からオファーが届くサイトです。
+        <br />
+        自信のあるゲームのレートを登録して、オファーを受け取りましょう。
+        <div style={{ height: 75, marginTop: 95 }}>
+          <a href="" className={styles.yellow}>
+            新規登録はこちらから
+          </a>
+        </div>
+        <div style={{ fontSize: 20 }}>およそ3分程度で作成することが出来ます。</div>
       </div>
       <div className={styles.detail}>
-        <div style={{ fontSize: 30, fontWeight: 'bold' }}>登録できるゲーム</div>
-        <div style={{ flexDirection: 'row', display: 'flex', gap: 30, marginTop: 30 }}>
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
+        <div style={{ fontSize: 40, fontWeight: 'bold', color: 'green' }}>登録できるゲーム</div>
+        <div style={{ flexDirection: 'row', display: 'flex', gap: 40, marginTop: 40 }}>
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
         </div>
-        <div style={{ flexDirection: 'row', display: 'flex', gap: 30, marginTop: 30 }}>
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
-          <div style={{ width: 120, height: 120, backgroundColor: 'black' }} />
+        <div style={{ flexDirection: 'row', display: 'flex', gap: 40, marginTop: 40 }}>
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
+          <div style={{ width: 140, height: 140, backgroundColor: 'black' }} />
         </div>
         <div style={{ height: 75, marginTop: 60 }}>
           <a href="" className={styles.yellow}>
-            新規登録はこちらから
+            ゲームの追加申請はこちらから
           </a>
         </div>
-        <div>およそ3分程度で作成することが出来ます。</div>
-      </div>
-      {/* メールアドレスとパスワードを入力する登録 */}
-      <div className={styles.submitarea}>
-        <div style={{ height: 75 }}>
-          <a href="" className={styles.yellow}>
-            新規登録はこちらから
-          </a>
-        </div>
-        <div>およそ3分程度で作成することが出来ます。</div>
+        <div style={{ fontSize: 24 }}>申請から運営が判断して実装します</div>
       </div>
       {/* ご利用の流れ */}
       <div className={styles.infoarea}>
-        <div>流れを書く</div>
+        <span style={{ fontSize: 40, fontWeight: 'bold', marginBottom: 15, color: 'green' }}>
+          採用までの流れ
+        </span>
+        <div className={styles.flow}>
+          <div className={styles.flowcard}>
+            <div className={styles.flowimg} />
+            <div className={styles.flowtitle}>1.登録</div>
+            <div className={styles.flowdetail}>
+              ゲームのレートや <br />
+              個人情報を登録します。
+            </div>
+          </div>
+          <div className={styles.flowcard}>
+            <div className={styles.flowimg} />
+            <div className={styles.flowtitle}>2.プロフィールの確認</div>
+            <div className={styles.flowdetail}>
+              各自情報を記入して <br />
+              スカウト率アップ
+            </div>
+          </div>
+          <div className={styles.flowcard}>
+            <div className={styles.flowimg} />
+            <div className={styles.flowtitle}>3.オファー</div>
+            <div className={styles.flowdetail}>企業からオファーが届きます</div>
+          </div>
+          <div className={styles.flowcard}>
+            <div className={styles.flowimg} />
+            <div className={styles.flowtitle}>4.採用</div>
+            <div className={styles.flowdetail}>人気の企業からの内定ゲット</div>
+          </div>
+        </div>
       </div>
     </>
   );
