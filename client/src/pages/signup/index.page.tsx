9d5ef65ffc3f212bd7e2b7aea7fb92e2eee15b90
s1f102101615 <s1f102101615@iniad.org>
2023-10-15 22:54:17 +0900

    個人情報入力ページ

--- client/src/pages/signup/index.page.tsx
+++ client/src/pages/signup/index.page.tsx
@@ -10,16 +10,66 @@ const Signup = () => {
   const [passwordtest, setPasswordtest] = useState('');
   const [isRegistered, setIsRegistered] = useState(false);
   const [isAccount, setIsAccount] = useState(false);
-  const [birthday, setBirthday] = useState('');
   const [address, setAddress] = useState('');
   const [education, setEducation] = useState('');
-  const [schoolType, setSchoolType] = useState('');
+  const [schooltype, setSchoolType] = useState('');
   const [schoolName, setSchoolName] = useState('');
   const [academicDiscipline, setAcademicDiscipline] = useState('');
   const [favoriteGame, setFavoriteGame] = useState('');
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
 
-  const handleBirthdayChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setBirthday(event.target.value);
+  // yearの選択肢
+  const currentYear = new Date().getFullYear();
+  const years = [];
+  for (let i = currentYear; i >= 1900; i--) {
+    years.push(i);
+  }
+
+  const schooldetail = [
+    '大学院',
+    '大学',
+    '短期大学',
+    '高等専門学校',
+    '専門学校',
+    '海外の学校',
+    '高等学校',
+    '中学校',
+    '小学校',
+    'その他',
+  ];
+
+  const academiclist = [
+    '情報・システム',
+    '数学・物理系',
+    '機械工学・電子工学',
+    '建築・土木・環境・材料・物質・資源',
+    '化学・生物学・科学・理学',
+    '医学・薬学・看護・保健',
+    '農・水産・畜産学',
+    '船舶・海洋・航空・宇宙',
+    'その他理系',
+    '文学・人文学・語学',
+    '経済学・経営学・商学系',
+    '法学・政治学・社会学・国際関係学',
+    'その他文系',
+    '教員養成・教育学',
+    '芸術学',
+    '生活科学・体育・栄養・健康福祉・住居',
+    '総合科学',
+  ];
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setYear(event.target.value);
+  };
+
+  const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setMonth(event.target.value);
+  };
+
+  const handleDayChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDay(event.target.value);
   };
 
   const handleAddressChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -30,7 +80,7 @@ const Signup = () => {
     setEducation(event.target.value);
   };
 
-  const handleSchoolTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSchooldetailChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSchoolType(event.target.value);
   };
 
@@ -38,7 +88,7 @@ const Signup = () => {
     setSchoolName(event.target.value);
   };
 
-  const handleAcademicDisciplineChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleAcademicDisciplineChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setAcademicDiscipline(event.target.value);
   };
 
@@ -162,8 +212,16 @@ const Signup = () => {
     return (
       <div>
         <Loginnow {...loginnowProps} />
-        <div style={{ paddingTop: 100 }}>
-          <div>仮登録が終了しました。</div>
+        <div className={styles.container} style={{ height: '78.55vh' }}>
+          <div className={styles.mainaccount} style={{ height: 540 }}>
+            <div>仮登録が完了</div>
+            <div>仮登録が完了しました。</div>
+            <div>メールをご確認ください</div>
+            <div>ご登録のメールアドレスに招待メールを送信しました。</div>
+            <div>メールに記述されているURLから本登録を完了してください。</div>
+            <div>*注意事項</div>
+            <div>まれに招待メールが「迷惑メール」扱いされる場合がございます。メールが届いていない場合、メールボックスの「ゴミ箱」および「迷惑メール」をご確認ください。</div>
+          </div>
         </div>
       </div>
     );
@@ -183,22 +241,79 @@ const Signup = () => {
             <div className={styles.mainaccount}>
               <div className={styles.form}>
                 <div className={styles.formGroup}>
-                  <div className={styles.title}>プロフィールを登録してください</div>
-                  {/* 生年月日、お住いの住所(都道府県)、就学情報、学校区分、学校名、学問系統
-                  学部学科、一番よくやるゲームなどを入力させる */}
+                  <div className={styles.title} style={{ fontSize: 26, marginBottom: 30 }}>
+                    プロフィールを登録してください
+                  </div>
                   <form onSubmit={handleSubmit}>
                     <div className={styles.formGroup}>
                       <label htmlFor="birthday" className={styles.label}>
                         生年月日
                       </label>
-                      <input
-                        type="text"
-                        id="birthday"
-                        name="birthday"
-                        value={birthday}
-                        onChange={handleBirthdayChange}
-                        className={styles.input}
-                      />
+                      {/* 年と月と日をselectから選ぶ */}
+                      <div>
+                        <select id="year" name="year" value={year} onChange={handleYearChange}>
+                          <option value="">--</option>
+                          {years.map((year) => (
+                            <option key={year} value={year}>
+                              {year}年
+                            </option>
+                          ))}
+                        </select>
+                        <label htmlFor="year">年</label>
+
+                        <select id="month" name="month" value={month} onChange={handleMonthChange}>
+                          <option value="">--</option>
+                          <option value="1">1月</option>
+                          <option value="2">2月</option>
+                          <option value="3">3月</option>
+                          <option value="4">4月</option>
+                          <option value="5">5月</option>
+                          <option value="6">6月</option>
+                          <option value="7">7月</option>
+                          <option value="8">8月</option>
+                          <option value="9">9月</option>
+                          <option value="10">10月</option>
+                          <option value="11">11月</option>
+                          <option value="12">12月</option>
+                        </select>
+                        <label htmlFor="month">月</label>
+
+                        <select id="day" name="day" value={day} onChange={handleDayChange}>
+                          <option value="">--</option>
+                          <option value="1">1日</option>
+                          <option value="2">2日</option>
+                          <option value="3">3日</option>
+                          <option value="4">4日</option>
+                          <option value="5">5日</option>
+                          <option value="6">6日</option>
+                          <option value="7">7日</option>
+                          <option value="8">8日</option>
+                          <option value="9">9日</option>
+                          <option value="10">10日</option>
+                          <option value="11">11日</option>
+                          <option value="12">12日</option>
+                          <option value="13">13日</option>
+                          <option value="14">14日</option>
+                          <option value="15">15日</option>
+                          <option value="16">16日</option>
+                          <option value="17">17日</option>
+                          <option value="18">18日</option>
+                          <option value="19">19日</option>
+                          <option value="20">20日</option>
+                          <option value="21">21日</option>
+                          <option value="22">22日</option>
+                          <option value="23">23日</option>
+                          <option value="24">24日</option>
+                          <option value="25">25日</option>
+                          <option value="26">26日</option>
+                          <option value="27">27日</option>
+                          <option value="28">28日</option>
+                          <option value="29">29日</option>
+                          <option value="30">30日</option>
+                          <option value="31">31日</option>
+                        </select>
+                        <label htmlFor="day">日</label>
+                      </div>
                     </div>
                     {/* 都道府県は47この選択肢から選ばせる */}
                     <div>
@@ -279,14 +394,19 @@ const Signup = () => {
                       <label htmlFor="schoolType" className={styles.label}>
                         学校区分
                       </label>
-                      <input
-                        type="text"
-                        id="schoolType"
-                        name="schoolType"
-                        value={schoolType}
-                        onChange={handleSchoolTypeChange}
-                        className={styles.input}
-                      />
+                      <select
+                        id="schooltype"
+                        name="schooltype"
+                        value={schooltype}
+                        onChange={handleSchooldetailChange}
+                      >
+                        <option value="">--</option>
+                        {schooldetail.map((detail) => (
+                          <option key={detail} value={detail}>
+                            {detail}
+                          </option>
+                        ))}
+                      </select>
                     </div>
                     <div className={styles.formGroup}>
                       <label htmlFor="schoolName" className={styles.label}>
@@ -305,14 +425,19 @@ const Signup = () => {
                       <label htmlFor="academicDiscipline" className={styles.label}>
                         学問系統・学部学科
                       </label>
-                      <input
-                        type="text"
+                      <select
                         id="academicDiscipline"
                         name="academicDiscipline"
                         value={academicDiscipline}
                         onChange={handleAcademicDisciplineChange}
-                        className={styles.input}
-                      />
+                      >
+                        <option value="">--</option>
+                        {academiclist.map((academic) => (
+                          <option key={academic} value={academic}>
+                            {academic}
+                          </option>
+                        ))}
+                      </select>
                     </div>
                     <div className={styles.formGroup}>
                       <label htmlFor="favoriteGame" className={styles.label}>
@@ -329,8 +454,14 @@ const Signup = () => {
                     </div>
                   </form>
                 </div>
-                <button onClick={handleback}>戻る</button>
-                <button onClick={handlenext}>次へ</button>
+                <div style={{ flexDirection: 'row', display: 'flex', gap: '40px' }}>
+                  <button onClick={handleback} className={styles.btn} style={{ marginTop: '20px' }}>
+                    戻る
+                  </button>
+                  <button onClick={handlenext} className={styles.btn} style={{ marginTop: '20px' }}>
+                    次へ
+                  </button>
+                </div>
               </div>
             </div>
           </div>
