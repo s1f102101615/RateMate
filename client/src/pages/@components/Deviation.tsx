c2eebf86408b98bf7e17650c0704cde4b8b209d9
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-10-16 00:53:47 +0900

    統合

---
+++ client/src/pages/@components/Deviation.tsx
@@ -0,0 +1,83 @@
+import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { erf } from 'mathjs';
+
+function ScoreBackground({ score }: { score: number }) {
+  const mean = 50; // 偏差値の平均
+  const stdDev = 10; // 偏差値の標準偏差
+
+  // 正規分布のCDFを計算
+  const cdf = 0.5 * (1 + erf((score - mean) / (stdDev * Math.sqrt(2))));
+  const calculatedHeight = `${cdf * 100}%`;
+
+  const styles = `
+  @keyframes riseFromBottom {
+    0% { bottom: -100%; }
+    100% { bottom: 0; }
+  }
+`;
+
+  return (
+    <Box
+      position="relative"
+      width="150px"
+      height="150px"
+      overflow="hidden"
+      bgcolor="#e0e0e0"
+      borderRadius="50%"
+      border={4}
+    >
+      <style>{styles}</style>
+      <Box
+        position="absolute"
+        bottom={0}
+        left={0}
+        right={0}
+        height={calculatedHeight}
+        bgcolor="primary.main"
+        style={{
+          animation: 'riseFromBottom 2s forwards',
+        }}
+      />
+      <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        style={{ transform: 'translate(-50%, -50%)' }}
+        color="white"
+        fontSize="40px"
+      >
+        {score}
+      </Box>
+    </Box>
+  );
+}
+
+export default function Deviation() {
+  const scores = {
+    ApexLegends: 32.0,
+    LeagueOfLegends: 60.0,
+    Valorant: 58.0,
+    Shadowverse: 65.0,
+  };
+
+  const highestScore = Math.max(...Object.values(scores));
+
+  return (
+    <Box padding={2} margin={2} bgcolor="white">
+      <Typography variant="h6" gutterBottom align="center">
+        ゲームレート
+      </Typography>
+      <Box display="flex" alignItems="center">
+        <ScoreBackground score={highestScore} />
+
+        <List>
+          {Object.entries(scores).map(([skill, score]) => (
+            <ListItem key={skill}>
+              <ListItemText primary={skill} secondary={`偏差値 ${score}`} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+}
---
+++ client/src/pages/company/index.page.tsx
@@ -0,0 +1,97 @@
+import { Box, Chip, Container, Grid, Paper, Typography } from '@mui/material';
+import { useAtom } from 'jotai';
+import { useState } from 'react';
+import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
+import { userAtom } from '../../atoms/user';
+import styles from './index.module.css';
+
+export default function Home() {
+  const [user] = useAtom(userAtom);
+
+  const [tags, setTags] = useState(['Python', 'Apache', 'AWS', 'Docker', 'GCP', 'フリーテキスト']);
+  const [title, setTitle] = useState(
+    `【速攻退職OK】 株式会社ほげんぽつ  バックエンドエンジニア募集`
+  );
+  const [salary, setSalary] = useState('バックエンドエンジニア 600万円~1,000万円');
+  const [description, setDescription] = useState(
+    '私たち「ほげんぽつ」は、2005年の設立以来、テクノロジーの力を活用して、企業や個人が直面する課題を解決するソリューションを提供してきました。特に、クラウドコンピューティング、AI、IoTの分野での実績が豊富であり、多くのクライアントから信頼を得ています。私たちのモットーは「技術で世界を変える」。日々進化するテクノロジーを駆使し、社会や人々の生活をより良くするための新しい価値を創出しています。経験豊富なエンジニアと専門家たちがチームを組み、お客様のニーズに応じた最適なソリューションを提案します。未来を共に切り開くパートナーとして、テックワールドをご信頼ください。'
+  );
+
+  const [subDescription, setSubDescription] = useState('がんばりましゅ');
+
+  const [requirements, setRequirements] = useState({
+    職種: 'バックエンドエンジニア',
+    必須スキル: 'Python, Django, RESTful APIの経験',
+    歓迎スキル: 'クラウドサービスの経験（AWS, GCPなど）',
+    勤務地: '東京都渋谷区',
+    給与: '月給 30万~50万円',
+    勤務時間: '9:00~18:00（フレックスタイム制）',
+    福利厚生: '健康保険完備、定期健康診断、スキルアップ研修制度',
+  });
+
+  return (
+    <div>
+      <BasicHeader user={user} />
+      <Container
+        maxWidth="lg"
+        className={styles.container}
+        sx={{
+          pt: 32,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom fontWeight="bold">
+          {salary}
+        </Typography>
+        <Box display="flex" gap={1} m={3}>
+          {tags.map((tag, index) => (
+            <Chip key={index} label={tag} variant="outlined" color="primary" />
+          ))}
+        </Box>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3}>
+              <Box padding={2}>
+                <Typography fontSize={17} lineHeight={2.1}>
+                  {description}
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3}>
+              <Box padding={2}>
+                <img src="/images/companyImage.jpg" width={530} height={300} alt="logo" />
+              </Box>
+              <Paper elevation={3}>
+                <Box padding={2}>
+                  <Typography variant="body1">{subDescription}</Typography>
+                </Box>
+              </Paper>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Box m={3}>
+          <Typography variant="h5" gutterBottom fontWeight="bold" align="center" mb={3}>
+            募集要項
+          </Typography>
+          <Grid container spacing={3}>
+            {Object.entries(requirements).map(([key, value], index) => (
+              <Grid item xs={12} md={6} key={index}>
+                <Typography variant="body1" fontWeight="bold">
+                  {key}
+                </Typography>
+                <Typography variant="body2">{value}</Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
+}
