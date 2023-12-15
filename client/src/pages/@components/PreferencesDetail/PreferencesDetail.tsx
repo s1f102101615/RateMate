63a2d32a540945a2fda08f52a42dfb9aeba8a738
s1f102101615 <s1f102101615@iniad.org>
2023-12-15 09:21:12 +0900

    preferences db post

--- client/src/pages/@components/PreferencesDetail/PreferencesDetail.tsx
+++ client/src/pages/@components/PreferencesDetail/PreferencesDetail.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 /* eslint-disable complexity */
 import { TextField } from '@mui/material';
 import type { Preference } from 'commonTypesWithClient/models';
@@ -14,18 +15,14 @@ const ProfileDetail: React.FC = () => {
   const [companySelectionType, setCompanySelectionType] = React.useState<string[]>([]);
   const [preferredLocations, setPreferredLocations] = React.useState<string[]>([]);
   const [preferredDetail, setPreferredDetail] = React.useState<string>('');
-  const [LargeCompany, setLargeCompany] = React.useState<boolean>(false);
-  const [MediumCompany, setMediumCompany] = React.useState<boolean>(false);
-  const [MegaVenture, setMegaVenture] = React.useState<boolean>(false);
-  const [Venture, setVenture] = React.useState<boolean>(false);
 
   const updateUserPreference = async () => {
     const userPreference: Preference = {
       userid: 'user123', // dammy
-      companySelection: ['company1', 'company2', 'company3'],
-      companySelectionType: ['companyType1', 'companyType2', 'companyType3', 'companyType4'],
-      preferredLocations: ['location1', 'location2', 'location3'],
-      preferredDetail: 'details',
+      companySelection,
+      companySelectionType,
+      preferredLocations,
+      preferredDetail,
     };
     await apiClient.preference.post({ body: userPreference });
   };
@@ -34,12 +31,6 @@ const ProfileDetail: React.FC = () => {
     const userPreference = await apiClient.preference.get();
     setCompanySelection(userPreference.body?.companySelection ?? []);
     setCompanySelectionType(userPreference.body?.companySelectionType ?? []);
-    setLargeCompany(userPreference.body?.companySelectionType?.includes('大手企業') ?? false);
-    setMediumCompany(userPreference.body?.companySelectionType?.includes('中小企業') ?? false);
-    setMegaVenture(
-      userPreference.body?.companySelectionType?.includes('メガベンチャー(500名以上～)') ?? false
-    );
-    setVenture(userPreference.body?.companySelectionType?.includes('ベンチャー企業') ?? false);
     setPreferredLocations(userPreference.body?.preferredLocations ?? []);
     setPreferredDetail(userPreference.body?.preferredDetail ?? '');
     console.log('u', userPreference);
@@ -58,6 +49,28 @@ const ProfileDetail: React.FC = () => {
     setEdit(false);
   };
 
+  const handleChange = (index: number, value: string) => {
+    setCompanySelection((prevValues) => {
+      const newValues = [...prevValues];
+      newValues[index] = value;
+      return newValues;
+    });
+  };
+
+  const handleTypeChange = (event: React.ChangeEvent<HTMLInputElement>, index: string) => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      setCompanySelectionType((prevItems) => [...prevItems, index]);
+    } else {
+      setCompanySelectionType((prevItems) => prevItems.filter((item) => item !== index));
+    }
+  };
+
+  const handleLocateChange = (index: number, value: string) => {
+    const newValues = [...preferredLocations];
+    newValues[index] = value;
+    setPreferredLocations(newValues);
+  };
   return (
     <>
       {edit ? (
@@ -73,40 +86,104 @@ const ProfileDetail: React.FC = () => {
             <div id="separated">
               <div className={styles.title}>企業選びで重視する事(3つまで)</div>
               <div>
-                <TextField label="姓" variant="outlined" style={{ margin: '16px 32px' }} />
-                <TextField label="名" variant="outlined" style={{ margin: '16px 32px' }} />
+                <TextField
+                  label="1"
+                  variant="outlined"
+                  style={{ margin: '16px 32px' }}
+                  value={companySelection[0]}
+                  onChange={(event) => handleChange(0, event.target.value)}
+                />
+                <TextField
+                  label="2"
+                  variant="outlined"
+                  style={{ margin: '16px 32px' }}
+                  value={companySelection[1]}
+                  onChange={(event) => handleChange(1, event.target.value)}
+                />
+                <TextField
+                  label="3"
+                  variant="outlined"
+                  style={{ margin: '16px 32px' }}
+                  value={companySelection[2]}
+                  onChange={(event) => handleChange(2, event.target.value)}
+                />
               </div>
               <div>
                 <div className={styles.title}>志望企業のタイプ</div>
                 {/* チェックボックス */}
                 <div style={{ flexDirection: 'column', display: 'flex', gap: 10, marginLeft: 30 }}>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('大手企業')}
+                      onChange={(event) => handleTypeChange(event, '大手企業')}
+                    />
                     <div>大手企業</div>
                   </div>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('中小企業')}
+                      onChange={(event) => handleTypeChange(event, '中小企業')}
+                    />
                     <div>中小企業</div>
                   </div>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('メガベンチャー(500名以上～)')}
+                      onChange={(event) => handleTypeChange(event, 'メガベンチャー(500名以上～)')}
+                    />
                     <div>メガベンチャー(500名以上～)</div>
                   </div>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('ベンチャー企業')}
+                      onChange={(event) => handleTypeChange(event, 'ベンチャー企業')}
+                    />
                     <div>ベンチャー企業</div>
                   </div>
                 </div>
               </div>
               <div className={styles.title}>希望勤務地(複数選択可)</div>
               <div>
-                <TextField label="姓" variant="outlined" style={{ margin: '16px 32px' }} />
-                <TextField label="名" variant="outlined" style={{ margin: '16px 32px' }} />
+                <TextField
+                  label="1"
+                  variant="outlined"
+                  style={{ margin: '16px 32px' }}
+                  value={preferredLocations[0]}
+                  onChange={(event) => handleLocateChange(0, event.target.value)}
+                />
+                <TextField
+                  label="2"
+                  variant="outlined"
+                  style={{ margin: '16px 32px' }}
+                  value={preferredLocations[1]}
+                  onChange={(event) => handleLocateChange(1, event.target.value)}
+                />
+                <TextField
+                  label="3"
+                  variant="outlined"
+                  style={{ margin: '16px 32px' }}
+                  value={preferredLocations[2]}
+                  onChange={(event) => handleLocateChange(2, event.target.value)}
+                />
               </div>
               <div className={styles.title}>希望詳細/その他</div>
               <div>
                 {/* テキストエリア */}
                 <textarea
+                  value={preferredDetail}
+                  onChange={(e) => setPreferredDetail(e.target.value)}
                   style={{
                     width: '600px',
                     height: '200px',
@@ -147,19 +224,39 @@ const ProfileDetail: React.FC = () => {
                 {/* チェックボックス */}
                 <div style={{ flexDirection: 'column', display: 'flex', gap: 10, marginLeft: 30 }}>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" checked={LargeCompany} />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('大手企業')}
+                    />
                     <div>大手企業</div>
                   </div>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" checked={MediumCompany} />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('中小企業')}
+                    />
                     <div>中小企業</div>
                   </div>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" checked={MegaVenture} />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('メガベンチャー(500名以上～)')}
+                    />
                     <div>メガベンチャー(500名以上～)</div>
                   </div>
                   <div style={{ flexDirection: 'row', display: 'flex' }}>
-                    <input type="checkbox" id="paper" name="paper" checked={Venture} />
+                    <input
+                      type="checkbox"
+                      id="paper"
+                      name="paper"
+                      checked={companySelectionType.includes('ベンチャー企業')}
+                    />
                     <div>ベンチャー企業</div>
                   </div>
                 </div>
