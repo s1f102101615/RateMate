a9b8fb791ad0a3b681bf599933a475b7ba2ae123
s1f102101615 <s1f102101615@iniad.org>
2023-12-19 11:45:56 +0900

    通知を表示

--- client/src/pages/notification/index.page.tsx
+++ client/src/pages/notification/index.page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-lines */
 import { useAtom } from 'jotai';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { apiClient } from 'src/utils/apiClient';
@@ -11,158 +12,173 @@ const Home = () => {
   const [user] = useAtom(userAtom);
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
+  const [notifications, setNotifications] = useState<
+    {
+      id: number;
+      userId: string;
+      title: string;
+      content: string;
+      url: string;
+      isRead: boolean;
+      createdAt: Date;
+    }[]
+  >(); // 通知のデータ構造(後で変更)
   const notificationsPerPage = 10;
 
   // 通知のデータ構造(後で変更)
   type Notification = {
-    icon: string;
+    id: number;
+    userId: string;
     title: string;
-    detail: string;
-    time: string;
+    content: string;
+    url: string;
+    isRead: boolean;
+    createdAt: Date;
   };
 
-  const setNotifications = async () => {
+  const setNotificationsFirst = async () => {
     // 通知のデータ（サーバーから取得すると仮定）
     const notification = await apiClient.notification.get();
+
+    setNotifications(notification.body);
     console.log(notification);
   };
 
   useEffect(() => {
-    setNotifications();
+    setNotificationsFirst();
   }, []);
 
   // 通知のデータ（サーバーから取得すると仮定）
-  const notifications: Notification[] = [
-    {
-      icon: 'https://example.com/icon1.png',
-      title: '株式会社cinraから「いいね」が届きました',
-      detail:
-        '【芸術文化をルーツとするメディアブランド】自社メディアとクライアントワークに携われるフロントエンジニアを募集！',
-      time: '2022年01月01日',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
-    {
-      icon: 'https://example.com/icon2.png',
-      title: 'タイトル2',
-      detail: '詳細2',
-      time: '2022-01-02 11:00:00',
-    },
+  // const notifications: Notification[] = [
+  //   {
+  //     icon: 'https://example.com/icon1.png',
+  //     title: '株式会社cinraから「いいね」が届きました',
+  //     detail:
+  //       '【芸術文化をルーツとするメディアブランド】自社メディアとクライアントワークに携われるフロントエンジニアを募集！',
+  //     time: '2022年01月01日',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
+  //   {
+  //     icon: 'https://example.com/icon2.png',
+  //     title: 'タイトル2',
+  //     detail: '詳細2',
+  //     time: '2022-01-02 11:00:00',
+  //   },
 
-    // 他の通知データ...
-  ];
+  // 他の通知データ...
 
   // 通知を表示するコンポーネント
   const NotificationItem: React.FC<{ notification: Notification }> = ({ notification }) => (
@@ -170,14 +186,17 @@ const Home = () => {
       {/* <img className={styles.icon} src={notification.icon} /> */}
       <div className={styles.icon} />
       <div className={styles.onebox}>
-        <div className={styles.title}>{notification.title}</div>
-        <div className={styles.detail}>{notification.detail}</div>
-        <div className={styles.time}>{notification.time}</div>
+        <Link href={notification.url}>
+          <div className={styles.title}>{notification.title}</div>
+        </Link>
+        <div className={styles.detail}>{notification.content}</div>
+        <div className={styles.time}>{notification.createdAt.toString()}</div>
       </div>
     </div>
   );
 
   // ページネーションを実装するコンポーネント
+  if (notifications === undefined) return <></>;
   const totalPages = Math.ceil(notifications.length / notificationsPerPage);
 
   // 現在のページの通知を取得
